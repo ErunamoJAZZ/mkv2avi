d756cc8aa773021c444b5d7e3eb02792db4ef19e
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-04-25 18:36:22 -0500

    Agregado un poco de saturación, y audio en bitrate variable (VBR).

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -23,8 +23,8 @@ echo "La salida será $salida.avi"
 
 #Codificación pasada 1
 mencoder $1 -vf scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
-fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=-150000:vhq=4:cartoon:chroma_me:chroma_opt:\
+hue=0:1.2,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+-ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:\
 hq_ac:trellis:bvhq=1:threads=2:pass=1:turbo \
 -nosound -ofps 24000/1001 -o /dev/null
 
@@ -38,10 +38,10 @@ Codificando la parte dos!!!!!!!!!!!!
 "
 #Codificación pasada 2
 mencoder $1 -vf scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
-fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=-150000:vhq=4:cartoon:chroma_me:chroma_opt:\
+hue=0:1.2,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+-ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:\
 hq_ac:trellis:bvhq=1:threads=2:pass=2 \
--oac mp3lame -lameopts cbr:br=192 -ofps 24000/1001 -o $salida.avi
+-oac mp3lame -lameopts vbr=2:preset=standard -ofps 24000/1001 -o $salida.avi
 
 #Tamaño video ~144MB, tamaño audio 33MB
 
