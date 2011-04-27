889b46526dc1ae671257c2a7c7ab0b985902fe13
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-04-26 19:30:52 -0500

    Hue (saturación), puesta antes de redimencionar.

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -1,6 +1,6 @@
 #Script hecho por ErunnamoJAZZ
 #@mail: anonimato1990@gmail.com
-#@version: 0.3.5
+#@version: 0.3.6
 #Ejemplo de uso:  ./mkv2avi VideoSource.mkv VideoSalida
 #Ojo, no poner el ".avi", el programa ya lo hace.
 
@@ -22,8 +22,8 @@ fi
 echo "La salida será $salida.avi"
 
 #Codificación pasada 1
-mencoder $1 -vf scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
-hue=0:1.2,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
+fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
 -ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:\
 hq_ac:trellis:bvhq=1:threads=2:pass=1:turbo \
 -nosound -ofps 24000/1001 -o /dev/null
@@ -37,10 +37,10 @@ Codificando la parte dos!!!!!!!!!!!!
 
 "
 #Codificación pasada 2
-mencoder $1 -vf scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
-hue=0:1.2,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
+fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
 -ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:\
-hq_ac:trellis:bvhq=1:threads=2:pass=2 \
+hq_ac:trellis:bvhq=1:threads=3:pass=2 \
 -oac mp3lame -lameopts vbr=2:preset=standard -ofps 24000/1001 -o $salida.avi
 
 #Tamaño video ~144MB, tamaño audio 33MB
