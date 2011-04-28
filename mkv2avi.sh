ffae81f9780e12fea21b3754c9865ceea5093223
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-04-28 17:07:39 -0500

    README ampliado. Modificaciones al codec xvid (guía MundoDivX.com).

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -1,14 +1,14 @@
 #Script hecho por ErunnamoJAZZ
 #@mail: anonimato1990@gmail.com
-#@version: 0.3.7
-#Ejemplo de uso:  ./mkv2avi VideoSource.mkv VideoSalida
+#@version: 0.3.8
+#Ejemplo de uso:  ./mkv2avi.sh VideoSource.mkv VideoSalida
 #Ojo, no poner el ".avi", el programa ya lo hace.
 
-echo $1
+echo 
 
 echo "
-
-Codificando la parte uno!!!!!!!!!!!!
+$1
+=======>>> Codificando la parte uno <<<=======
 
 
 "
@@ -22,31 +22,31 @@ fi
 echo "La salida será $salida.avi"
 
 #Codificación pasada 1
-mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
-fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts vhq=4:cartoon:chroma_me:chroma_opt:\
-hq_ac:trellis:bvhq=1:kfreduction=23:threads=2:pass=1:turbo \
+mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
+hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+-ovc xvid -xvidencopts vhq=4:cartoon:chroma_me:chroma_opt:hq_ac:trellis:\
+bvhq=1:bquant_ratio=162:bquant_offset=0:threads=2:pass=1:turbo \
 -nosound -ofps 24000/1001 -o /dev/null
 
 echo "
 
 
-Codificando la parte dos!!!!!!!!!!!!
+=======>>> Codificando la parte dos <<<=======
 
 
 
 "
 #Codificación pasada 2
-mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,hqdn3d,\
-fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:\
-hq_ac:trellis:bvhq=1:kfreduction=23:threads=3:pass=2 \
+mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
+hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+-ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:hq_ac:\
+trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:threads=3:pass=2 \
 -oac mp3lame -lameopts vbr=2:preset=standard -ofps 24000/1001 -o $salida.avi
 
-#Tamaño video ~144MB, tamaño audio 33MB
+#Tamaño video ~170MB, tamaño audio ~33MB
 
 echo "
 
 
-Fin n.n"
+=======>>>  Fin n.n  <<<======="
 rm divx2pass.log
