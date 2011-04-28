38954eda792b381756aad07fa130fd72d8e692ce
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-04-28 18:05:45 -0500

    Cambio de algoritmo VBR del audio, a ABR.

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -41,7 +41,7 @@ mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
 hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
 -ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:hq_ac:\
 trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:threads=3:pass=2 \
--oac mp3lame -lameopts vbr=2:preset=standard -ofps 24000/1001 -o $salida.avi
+-oac mp3lame -lameopts vbr=3:preset=standard -ofps 24000/1001 -o $salida.avi
 
 #Tamaño video ~170MB, tamaño audio ~33MB
 
