a8b1482042939631bb31a2b8f585f32cccd5217d
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-04-29 00:41:04 -0500

    Ahora sin cartoon mode. Queda mucho mejor con los animes actuales.

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -1,6 +1,6 @@
 #Script hecho por ErunnamoJAZZ
 #@mail: anonimato1990@gmail.com
-#@version: 0.3.8
+#@version: 0.4.0
 #Ejemplo de uso:  ./mkv2avi.sh VideoSource.mkv VideoSalida
 #Ojo, no poner el ".avi", el programa ya lo hace.
 
@@ -24,8 +24,9 @@ echo "La salida será $salida.avi"
 #Codificación pasada 1
 mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
 hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts vhq=4:cartoon:chroma_me:chroma_opt:hq_ac:trellis:\
-bvhq=1:bquant_ratio=162:bquant_offset=0:threads=2:pass=1:turbo \
+-ovc xvid -xvidencopts bitrate=1100:vhq=4:chroma_me:chroma_opt:hq_ac:\
+trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:packed:quant_type=h263:\
+lumi_mask:threads=2:pass=1:turbo \
 -nosound -ofps 24000/1001 -o /dev/null
 
 echo "
@@ -39,11 +40,13 @@ echo "
 #Codificación pasada 2
 mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
 hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=950:vhq=4:cartoon:chroma_me:chroma_opt:hq_ac:\
-trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:threads=3:pass=2 \
+-ovc xvid -xvidencopts bitrate=1100:vhq=4:chroma_me:chroma_opt:\
+hq_ac:trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:packed:quant_type=h263:\
+curve_compression_high=0:curve_compression_low=15:keyframe_boost=10:\
+kfthreshold=1:kfreduction=20:lumi_mask:threads=3:pass=2 \
 -oac mp3lame -lameopts vbr=3:preset=standard -ofps 24000/1001 -o $salida.avi
 
-#Tamaño video ~170MB, tamaño audio ~33MB
+#Tamaño video ~175MB, tamaño audio ~25MB
 
 echo "
 
