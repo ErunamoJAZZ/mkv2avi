80042db4a05ddac1db92296e9789056cd247eacf
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-04-29 13:48:51 -0500

    Bitrate a 950kps, y threads a 0 (automatico).

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -24,14 +24,16 @@ echo "La salida será $salida.avi"
 #Codificación pasada 1
 mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
 hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=1100:vhq=4:chroma_me:chroma_opt:hq_ac:\
+-ovc xvid -xvidencopts bitrate=950:vhq=4:chroma_me:chroma_opt:hq_ac:\
 trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:packed:quant_type=h263:\
-lumi_mask:threads=2:pass=1:turbo \
+lumi_mask:threads=0:pass=1:turbo \
 -nosound -ofps 24000/1001 -o /dev/null
 
 echo "
 
 
+
+$1
 =======>>> Codificando la parte dos <<<=======
 
 
@@ -40,10 +42,10 @@ echo "
 #Codificación pasada 2
 mencoder $1 -vf hue=0:1.2,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
 hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=1100:vhq=4:chroma_me:chroma_opt:\
+-ovc xvid -xvidencopts bitrate=950:vhq=4:chroma_me:chroma_opt:\
 hq_ac:trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:packed:quant_type=h263:\
 curve_compression_high=0:curve_compression_low=15:keyframe_boost=10:\
-kfthreshold=1:kfreduction=20:lumi_mask:threads=3:pass=2 \
+kfthreshold=1:kfreduction=20:lumi_mask:threads=0:pass=2 \
 -oac mp3lame -lameopts vbr=3:preset=standard -ofps 24000/1001 -o $salida.avi
 
 #Tamaño video ~175MB, tamaño audio ~25MB
