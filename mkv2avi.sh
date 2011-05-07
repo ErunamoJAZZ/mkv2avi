61e5a4108b694bd437cb4e53f547b97e45ca8fee
C. Daniel Sanchez R. ErunamoJAZZ <anonimato1990@gmail.com>
2011-05-07 12:15:43 -0500

    hqdn3d se ejecuta de ultimo (después de eq [contraste]).

--- mkv2avi.sh
+++ mkv2avi.sh
@@ -23,9 +23,9 @@ fi
 echo "La salida será $salida.avi"
 
 #Codificación pasada 1
-mencoder $1 -vf scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
-hqdn3d,eq=0:5,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=-175000:vhq=4:chroma_me:chroma_opt:hq_ac:\
+mencoder $1 -vf hue=0.0:1.1,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
+eq=0:5,hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+-ovc xvid -xvidencopts bitrate=1000:vhq=4:chroma_me:chroma_opt:hq_ac:\
 trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:packed:quant_type=h263:\
 lumi_mask:threads=2:pass=1:turbo \
 -nosound -ofps 24000/1001 -o /dev/null
@@ -41,9 +41,9 @@ $1
 
 "
 #Codificación pasada 2
-mencoder $1 -vf scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
-hqdn3d,eq=0:5,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
--ovc xvid -xvidencopts bitrate=-175000:vhq=4:chroma_me:chroma_opt:\
+mencoder $1 -vf hue=0.0:1.1,scale=704:396:param=3,unsharp=c3x3:0.5:l3x3:0.2,\
+eq=0:5,hqdn3d,fixpts=fps=24000/1001,ass,fixpts -ass -sws 9 -noskip \
+-ovc xvid -xvidencopts bitrate=1000:vhq=4:chroma_me:chroma_opt:\
 hq_ac:trellis:bvhq=1:bquant_ratio=162:bquant_offset=0:packed:quant_type=h263:\
 curve_compression_high=0:curve_compression_low=15:keyframe_boost=10:\
 kfthreshold=1:kfreduction=20:lumi_mask:threads=3:pass=2 \
